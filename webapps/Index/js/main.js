3aa1f7b46e8d7fb9909cb29f70e7f3450188515a
aiwei1224 <aiwei_96@live.com>
2019-11-16 19:49:06 +0800

    change

---
+++ webapps/Index/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+ $('.sidenav').sidenav();
+});
+
+  $(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
+AOS.init();
